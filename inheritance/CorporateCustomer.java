148fa6969159be2ea07292bb7fc6e951ffd4ec11
Osman Eyser Engin <95635239+eyserengin@users.noreply.github.com>
2022-08-25 16:03:20 +0300

    Update CorporateCustomer.java

--- inheritance/CorporateCustomer.java
+++ inheritance/CorporateCustomer.java
@@ -6,10 +6,5 @@ public class CorporateCustomer extends Customer {
     String taxNumber;
 
 
-//    public CorporateCustomer(String companyName, String taxNumber){
-//        super();
-//        this.companyName = companyName;
-//        this.taxNumber = taxNumber;
-//
-//    }
+
 }
