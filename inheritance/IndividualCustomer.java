e70df0acd306fb7206ab5a273d9a3418dd0774d7
Osman Eyser Engin <95635239+eyserengin@users.noreply.github.com>
2022-08-25 16:02:41 +0300

    Update IndividualCustomer.java

--- inheritance/IndividualCustomer.java
+++ inheritance/IndividualCustomer.java
@@ -5,10 +5,4 @@ public class IndividualCustomer extends Customer{
     String lastName;
     String nationalIdentity;
 
-//    public IndividualCustomer(String firstName,String lastName, String nationalIdentity){
-//        super(int id, String customerNumber);
-//        this.firstName = firstName;
-//        this.lastName = lastName;
-//        this.nationalIdentity = nationalIdentity;
-//    }
 }
